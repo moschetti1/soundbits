07c3710757c029606bd7566a95569992aa6d331c
moschetti1 <francisco.moschetti@gmail.com>
2024-08-12 22:22:15 -0300

    legal txt

--- src/twitch_bot/urls.py
+++ src/twitch_bot/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from main import views
-from .views import login_template, home
+from .views import login_template, home, privacy_policy, terms_of_service, refund_policy
 from billing import views as billing_views
 urlpatterns = [
     path("", home, name="home"),
@@ -12,7 +12,10 @@ urlpatterns = [
     path("hooks/twitch/events/", views.twitch_eventsub_callback, name="twitch-hook"),
     path("hooks/lemon/events/", billing_views.lemon_webhook, name="lemon-webhook"),
     path("overlay/<str:user_id>/", views.broadcaster_overlay, name="overlay"),
-    path("dashboard/", include(("main.urls", "main"), namespace="dashboard"))
+    path("dashboard/", include(("main.urls", "main"), namespace="dashboard")),
+    path("legal/refunds-policy/", refund_policy, name="refunds-policy"),
+    path("legal/privacy-policy/", privacy_policy, name="privacy-policy"),
+    path("legal/terms-of-service/", terms_of_service, name="terms-of-service"),
 ]
 
 
--- src/twitch_bot/views.py
+++ src/twitch_bot/views.py
@@ -8,3 +8,12 @@ def login_template(request):
     if request.user.is_authenticated:
         return redirect("dashboard:overview")
     return render(request, "accounts/login.html")
+
+def refund_policy(request):
+    return render(request, "legal/refunds.html")
+
+def privacy_policy(request):
+    return render(request, "legal/privacy.html")
+
+def terms_of_service(request):
+    return render(request, "legal/tos.html")
